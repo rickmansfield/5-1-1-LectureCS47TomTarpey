5921c6a471a77621addabcb4aae70d74d733252b
Rick <ricksmycodeguy@gmail.com>
2021-10-21 19:21:57 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+print("Hello from the colab!") #  Hello from the colab!
+
+# Let's print some things to the screen!
+print("Hello to CS47!") #  Hello to CS47!
+print("some other stuff") # some other stuff
+
+
+# lets look at some types
+
+print(type(123))  # <class 'int'>
+print(type(12.9)) #  <class 'float'>
+print(type("Hello")) # <class 'str'>
+print(type([1, 2, 3])) #  <class 'list'>
+print(type({"a": 12, "b": 22})) #  <class 'dict'>
+
+# basic operators
+"""
+= # assignment
++= 1 # increment
+-= 1 # decrement
+*
+*=
+/=
+/
+%
+"""
+
+a = 12
+b = 10
+print(a) #  12
+a += b
+print(a) # 22
+
+a *= b
+print(a) # 220
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Intro to Python I
+
+- Some Basics before we get in to the language
+- creating a python file
+- directories?
+- using python in the shell
+- using the python REPL
+- using python in a notebook
+
+## Some Objectives
+Name and describe the details of each step of Lambda's Problem-Solving Framework (U.P.E.R.)
+-Use a print statement
+- use white space to denote blocks
+- use basic types
+- use basic operators
+- create f strings
+- perform basic string operations
+- evaluate conditional operations
+- use for and while loops
+- create user defined functions and call them
