505eafa848a1f179e09ba2fd4c7d70eb00aebc5f
Rick <ricksmycodeguy@gmail.com>
2021-10-23 11:46:26 -0500

    created FastAsPossible.py for matching video

---
+++ FastAsPossible.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=VchuKL44s6E
+
+x = {'someKey': 4}
+
+for key, value in x.items():
+    print(key, value)
+
+for eachValue in x.items():
+    print(key, value)
+    print(key, x[key])
